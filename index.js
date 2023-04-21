9041e7d8653f12f07db8ab3ab5fc626605b21cc8
Oskulindholm <lindholm.osku@gmail.com>
2023-04-21 12:04:31 +0300

    Tehtävä 3.4

--- index.js
+++ index.js
@@ -52,6 +52,12 @@ app.get('/api/persons/:id', (req, res) => {
     res.status(404).end()
   }
 })
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(p => p.id !== id)  
+  res.status(204).end()
+})
   
 
 const PORT = 3001
