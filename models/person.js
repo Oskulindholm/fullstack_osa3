b518d8a12992bb14739ea5ed32024d1790e50f30
Oskulindholm <lindholm.osku@gmail.com>
2023-04-24 14:58:28 +0300

    Tehtävä 3.13.2

--- models/person.js
+++ models/person.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
 
-const url = `${process.env.MONGODB_URI}`
+//const url = `${process.env.MONGODB_URI}`
+const url = process.env.MONGODB_URI
 
+console.log(process.env.MONGODB_URI)
 console.log('connecting to', url)
 
 mongoose.connect(url)
