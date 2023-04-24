2f4e09c36913dadd1bd8cb866a03bd9257d57345
Oskulindholm <lindholm.osku@gmail.com>
2023-04-24 17:39:11 +0300

    Tehtävä 3.16

--- index.js
+++ index.js
@@ -14,33 +14,7 @@ app.use(express.static('build'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
 
-let persons = [
-    {
-      "name": "Arto Hellas",
-      "number": "040-123456",
-      "id": 1
-    },
-    {
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523",
-      "id": 2
-    },
-    {
-      "name": "Dan Abramov",
-      "number": "12-43-234345",
-      "id": 3
-    },
-    {
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122",
-      "id": 4
-    },
-    {
-      "name": "Aku Ankka",
-      "number": "313",
-      "id": 5
-    }
-  ]
+let persons = []
 
 const generateId = () => {
   return Math.floor(Math.random() * 1000)
@@ -65,15 +39,17 @@ app.get('/api/persons', (req, res) => {
   })
 })
 
-app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find(p => p.id === id)
-
-  if (person) {
-    res.json(person)
-  } else {
-    res.status(404).end()
-  }
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id).then( person => {
+    if (person) {
+      res.json(person)
+    } else {
+      res.status(404).end()
+    }
+  })
+  .catch(error => {
+    console.log(error)
+    next(error)})
 })
 
 app.post('/api/persons', (req, res) => {
@@ -110,12 +86,33 @@ app.post('/api/persons', (req, res) => {
   console.log(person)
 })
 
-app.delete('/api/persons/:id', (req, res) => {
+app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndRemove(req.params.id).then(result => {
     res.status(204).end()
   })
   .catch(error => next(error))
 })
+
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'Unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+
+
+const errorHandler = (err, req, res, next) => {
+  console.error(err.message)
+
+  if (err.name === 'CastError') {
+    {
+      res.status(400).send({ error: 'Malformatted id' })
+    }
+  }
+
+  next(err)
+}
+app.use(errorHandler)
   
 
 const PORT = process.env.PORT
