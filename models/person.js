c33a587440d6147f553ba5ade22b26c00b30f2fd
Oskulindholm <lindholm.osku@gmail.com>
2024-04-17 10:57:25 +0300

    Tehtävä 3.20

--- models/person.js
+++ models/person.js
@@ -30,7 +30,7 @@ const personSchema = new mongoose.Schema({
     minlength: 8,
     validate: {
       validator: function(v) {
-        return /\d{2,3}-\d{+}/.test(v)
+        return /^(\d{2,3}-\d+)$/.test(v)
       },
       message: "The number is malformatted"
     },
